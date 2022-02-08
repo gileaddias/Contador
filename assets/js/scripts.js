a75a25b3a8370cba0c7e184dbbdfe7408bde4c4d
Gileaddias <gfarias.gpf@gmail.com>
2022-02-08 16:33:52 -0300

    Atualizando Javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello Word");
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber 1 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
